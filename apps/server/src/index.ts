9e35936e26b9a5d6ea2d2ea6a46ffda6c06f3cb3
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2024-12-31 21:40:05 +0530

    added awsroutes

---
+++ apps/server/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import awsRoutes from './aws/awsRoutes';
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/aws', awsRoutes);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
