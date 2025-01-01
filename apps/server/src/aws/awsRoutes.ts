9ff9f49baf50c597733df416f4333e130f3cd2d6
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2025-01-01 13:14:46 +0530

    added drag and drop

--- apps/server/src/aws/awsRoutes.ts
+++ apps/server/src/aws/awsRoutes.ts
@@ -1,5 +1,5 @@
 import { Router} from "express";
-import { getObjectURL, listObjects, putObject } from "./awsController";
+import { copyObject, deleteObject, getObjectURL, listObjects, putObject } from "./awsController";
 
 
 
@@ -37,7 +37,53 @@ awsRoutes.post("/putObject",async (req, res) => {
         }
     }  
 );
+awsRoutes.post("/moveObject", async (req, res) => {
+    const { sourceKey, destinationKey } = req.body;
 
+    // Validate input
+    if (
+        !sourceKey || typeof sourceKey !== "string" ||
+        !destinationKey || typeof destinationKey !== "string"
+    ) {
+        return res.status(400).json({ error: "SourceKey and DestinationKey are required and must be strings" });
+    }
+
+    try {
+        // Step 1: Copy the object to the new location
+        await copyObject(
+            sourceKey,
+            destinationKey
+        );
+
+        // Step 2: Delete the object from the old location
+        await deleteObject(
+           sourceKey
+        )
+
+        // Step 3: Return the new URL or success message
+        // const newUrl = https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${destinationKey};
+        return res.status(200).json({ message: "Object moved successfully" });
+    } catch (error) {
+        console.error("Error moving object:", error);
+        return res.status(500).json({ error: "Failed to move object" });
+    }
+});
+
+awsRoutes.post("/deleteObject",async (req, res) => {
+        const { path } = req.body;
+
+        // Validate the path
+        if (!path || typeof path !== "string") {
+            return res.status(400).json({ error: "Path is required and must be a string" });
+        }
+
+        try {
+            const result = await deleteObject(path);
+            return res.status(200).json({ result });
+        } catch (error) {
+            return res.status(500).json({ error: "Failed to delete object" });
+        }
+    });
 awsRoutes.post("/listObjects",async (req, res) => {
         const { path } = req.body;
 
